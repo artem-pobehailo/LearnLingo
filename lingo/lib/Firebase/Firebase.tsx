08b8b7861e933f5739f0856234991df1c360e650
ARTEM POBEHAILO <partemishe@gmail.com>
2026-02-18 15:49:43 +0200

    fix env

--- lingo/lib/Firebase/Firebase.tsx
+++ lingo/lib/Firebase/Firebase.tsx
@@ -3,7 +3,7 @@ import { getAuth } from 'firebase/auth';
 import { Database, getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: 'learn-lingo-artem.firebaseapp.com',
   databaseURL: 'https://learn-lingo-artem-default-rtdb.firebaseio.com',
   projectId: 'learn-lingo-artem',
